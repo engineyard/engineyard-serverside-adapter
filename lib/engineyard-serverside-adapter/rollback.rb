d01625b99e3a43953e10fbe084829e686d6b799d
Sam Merritt & Martin Emde <memde@engineyard.com>
2010-09-15 16:41:07 -0700

    Rollback actually requires framework_env

--- lib/engineyard-serverside-adapter/rollback.rb
+++ lib/engineyard-serverside-adapter/rollback.rb
@@ -3,11 +3,12 @@ module EY
     class Adapter
       class Rollback < Action
 
-        option :app,       :string,    :required => true
-        option :stack,     :string,    :required => true
-        option :instances, :instances, :required => true
-        option :config,    :json
-        option :verbose,   :boolean
+        option :app,           :string,    :required => true
+        option :config,        :json
+        option :framework_env, :string,    :required => true
+        option :instances,     :instances, :required => true
+        option :stack,         :string,    :required => true
+        option :verbose,       :boolean
 
       private
 
--- spec/rollback_spec.rb
+++ spec/rollback_spec.rb
@@ -4,21 +4,24 @@ describe EY::Serverside::Adapter::Rollback do
   it_should_behave_like "it installs engineyard-serverside"
 
   it_should_behave_like "it accepts app"
+  it_should_behave_like "it accepts framework_env"
   it_should_behave_like "it accepts instances"
   it_should_behave_like "it accepts stack"
   it_should_behave_like "it accepts verbose"
 
   it_should_require :app
-  it_should_require :stack
+  it_should_require :framework_env
   it_should_require :instances
+  it_should_require :stack
 
   context "with valid arguments" do
     let(:command) do
       adapter = described_class.new do |arguments|
-        arguments.app = "rackapp"
-        arguments.instances = [{:hostname => 'localhost', :roles => %w[han solo], :name => 'chewie'}]
-        arguments.stack = "nginx_unicorn"
-        arguments.config = {'a' => 1}
+        arguments.app           = "rackapp"
+        arguments.framework_env = 'production'
+        arguments.instances     = [{:hostname => 'localhost', :roles => %w[han solo], :name => 'chewie'}]
+        arguments.stack         = "nginx_unicorn"
+        arguments.config        = {'a' => 1}
       end
       last_command(adapter)
     end
@@ -28,7 +31,7 @@ describe EY::Serverside::Adapter::Rollback do
     end
 
     it "invokes exactly the right command" do
-      command.should == "engineyard-serverside _#{EY::Serverside::Adapter::VERSION}_ deploy rollback --app rackapp --config '{\"a\":1}' --instance-names localhost:chewie --instance-roles localhost:han,solo --instances localhost --stack nginx_unicorn"
+      command.should == "engineyard-serverside _#{EY::Serverside::Adapter::VERSION}_ deploy rollback --app rackapp --config '{\"a\":1}' --framework-env production --instance-names localhost:chewie --instance-roles localhost:han,solo --instances localhost --stack nginx_unicorn"
     end
   end
 end
