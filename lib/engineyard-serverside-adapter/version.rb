ce045895f4293957f3a45807e4a1b75cde4f8b35
Sam Merritt <spam@andcheese.org>
2010-10-15 16:16:24 -0700

    Bump to engineyard-serverside version 1.3.5

--- lib/engineyard-serverside-adapter/version.rb
+++ lib/engineyard-serverside-adapter/version.rb
@@ -1,7 +1,7 @@
-module EY
-  module Serverside
-    class Adapter
-      VERSION = "1.3.4"
+  module EY
+    module Serverside
+      class Adapter
+        VERSION = "1.3.5"
+      end
     end
   end
-end
