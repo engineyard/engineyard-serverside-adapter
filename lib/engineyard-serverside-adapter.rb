442ccd24541437fe7ea1590247c8accef54fa9c6
Jason Hansen & Josh Lane <jhansen@engineyard.com>
2012-03-23 13:42:53 -0700

    don't limit serverside version to major.minor

--- lib/engineyard-serverside-adapter.rb
+++ lib/engineyard-serverside-adapter.rb
@@ -14,10 +14,7 @@ module EY
       autoload :Rollback,               'engineyard-serverside-adapter/rollback'
       autoload :VERSION,                'engineyard-serverside-adapter/version'
 
-      # engineyard-serverside uses major.minor.patch; using a
-      # potentially-4-digit version lets us release fixes in the
-      # adapter while still keeping in version sync
-      ENGINEYARD_SERVERSIDE_VERSION = ENV['ENGINEYARD_SERVERSIDE_VERSION'] || VERSION.split('.')[0..2].join('.')
+      ENGINEYARD_SERVERSIDE_VERSION = ENV['ENGINEYARD_SERVERSIDE_VERSION'] || VERSION
 
       def initialize(gem_bin_path = "")
         @gem_bin_path = Pathname.new(gem_bin_path)
