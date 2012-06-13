852b42aed1ff220984a1d95203de332c95d84072
Martin Emde <martin.emde@gmail.com>
2012-06-13 14:39:35 -0700

    Bump for prerelease 2.0.0.pre4

--- lib/engineyard-serverside-adapter/version.rb
+++ lib/engineyard-serverside-adapter/version.rb
@@ -1,8 +1,8 @@
 module EY
   module Serverside
     class Adapter
-      VERSION = "2.0.0.pre3"
-      ENGINEYARD_SERVERSIDE_VERSION = ENV['ENGINEYARD_SERVERSIDE_VERSION'] || "2.0.0.pre3"
+      VERSION = "2.0.0.pre4"
+      ENGINEYARD_SERVERSIDE_VERSION = ENV['ENGINEYARD_SERVERSIDE_VERSION'] || "2.0.0.pre4"
     end
   end
 end
