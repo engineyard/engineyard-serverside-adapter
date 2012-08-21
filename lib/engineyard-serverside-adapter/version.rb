bb841a1869a9d97b5dbcd7ee6b61759ec1a36734
Martin Emde <martin.emde@gmail.com>
2012-08-21 15:54:06 -0700

    Bump to engineyard-serverside version 2.0.1

--- lib/engineyard-serverside-adapter/version.rb
+++ lib/engineyard-serverside-adapter/version.rb
@@ -2,7 +2,7 @@ module EY
   module Serverside
     class Adapter
       VERSION = "2.0.1"
-      ENGINEYARD_SERVERSIDE_VERSION = ENV['ENGINEYARD_SERVERSIDE_VERSION'] || VERSION
+      ENGINEYARD_SERVERSIDE_VERSION = ENV['ENGINEYARD_SERVERSIDE_VERSION'] || "2.0.1"
     end
   end
 end
