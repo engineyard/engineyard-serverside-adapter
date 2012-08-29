914708510e7d27b3ecd24be8128c383b4b9e81d8
Martin Emde & Jacob Burkhart <memde@engineyard.com>
2012-08-28 18:16:08 -0700

    Add .pre for next release

--- lib/engineyard-serverside-adapter/version.rb
+++ lib/engineyard-serverside-adapter/version.rb
@@ -1,7 +1,10 @@
 module EY
   module Serverside
     class Adapter
-      VERSION = "2.0.1"
+      VERSION = "2.0.2.pre"
+      # For backwards compatibility, the serverside version default will be maintained until 2.1
+      # It is recommended that you supply a serverside_version to engineyard-serverside-adapter
+      # rather than relying on the default version here. This default will go away soon.
       ENGINEYARD_SERVERSIDE_VERSION = ENV['ENGINEYARD_SERVERSIDE_VERSION'] || "2.0.1"
     end
   end
