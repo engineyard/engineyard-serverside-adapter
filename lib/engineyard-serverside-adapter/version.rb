1c3d99700a6313c578682df086334d40f43e7d1a
Martin Emde <martin.emde@gmail.com>
2012-08-30 14:10:24 -0700

    Bump to version 2.0.3

--- lib/engineyard-serverside-adapter/version.rb
+++ lib/engineyard-serverside-adapter/version.rb
@@ -1,7 +1,7 @@
 module EY
   module Serverside
     class Adapter
-      VERSION = "2.0.3.pre"
+      VERSION = "2.0.3"
       # For backwards compatibility, the serverside version default will be maintained until 2.1
       # It is recommended that you supply a serverside_version to engineyard-serverside-adapter
       # rather than relying on the default version here. This default will go away soon.
